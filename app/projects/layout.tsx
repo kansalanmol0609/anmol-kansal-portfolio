3a04e585a513b81b7bc1207844aff7d154883e70
Anmol Kansal <kansalanmol0609@gmail.com>
2023-12-26 00:38:35 +0530

    fix: fix build issue

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,45 +1,7 @@
-"use client";
+export const metadata = {
+	title: `Projects`
+};
 
-import { Suspense, useMemo, useState } from "react";
-import { HeadingDivider, Loader } from "components";
-import { Filter } from "./components/Filter";
-import { Projects } from "./components/Projects";
-import { PROJECTS } from "constants/projects";
-
-export default function Page() {
-	const [category, setCategory] = useState<string | undefined>(undefined);
-
-	const filteredProjects = useMemo(
-		() => PROJECTS.filter((project) => !category || project.category === category),
-		[category]
-	);
-
-	const onClick = (catName: string | undefined) => setCategory(catName);
-
-	return (
-		<div className="container-md">
-			<section id="projects" className="section">
-				<HeadingDivider title="Relevant projects" />
-
-				<Filter onClick={onClick} />
-
-				<Suspense
-					fallback={
-						<div className="flex-center">
-							<Loader />
-						</div>
-					}
-				>
-					{filteredProjects.length === 0 ? (
-						// Empty state
-						<div className="flex-center">
-							<h3 className="text-2xl">No projects found in {category} category</h3>
-						</div>
-					) : (
-						<Projects projects={filteredProjects} />
-					)}
-				</Suspense>
-			</section>
-		</div>
-	);
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return <div className="container-md">{children}</div>;
 }
